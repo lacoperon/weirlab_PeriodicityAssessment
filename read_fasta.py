3286b83df368b7ec54817448150ad20b23c170cf
Elliot <elliot.james.williams@gmail.com>
2018-03-01 11:03:31 -0500

    Updated read_fasta to write file associated with fasta values to csv; also counts background nucleotide frequencies of all read in genes

--- read_fasta.py
+++ read_fasta.py
@@ -13,6 +13,10 @@ def read_fasta_and_filter(fasta_file, gene_file):
         hit_counter = 0
         gene_symbol = ""
 
+        # This variable concatenates the dataset of sequences, obtaining
+        # background nucleotide frequencies for the human genome
+        total_sequence_aggregator = ""
+
         # Initializes dict containing counts for each gene_symbol
         gene_symbol_counter = {}
         for gene in gene_symbols:
@@ -27,7 +31,7 @@ def read_fasta_and_filter(fasta_file, gene_file):
                 # header, sequence pair left to write
                 if header and sequence and gene_symbol.strip() in gene_symbols:
                     # We write the unwritten pair, if it should be written
-                    fasta_df.append([header, sequence])
+                    fasta_df.append([header, sequence, gene_symbol])
                     hit_counter += 1
                 # Then we exit the loop
                 break
@@ -36,7 +40,7 @@ def read_fasta_and_filter(fasta_file, gene_file):
             if line[0] == ">":
                 # If appropriate, we write the header and sequence to our list
                 if header and sequence and gene_symbol.strip() in gene_symbols:
-                    fasta_df.append([header, sequence])
+                    fasta_df.append([header, sequence, gene_symbol])
                     hit_counter += 1
                     gene_symbol_counter[gene_symbol] += 1
 
@@ -49,9 +53,10 @@ def read_fasta_and_filter(fasta_file, gene_file):
             # Adding to sequence
             else:
                 sequence += re.sub("\s", "", line)
+                total_sequence_aggregator += re.sub("\s", "", line)
 
         # Printing out metadata for run to stdout
-        print("Number of hits is {}".format(hit_counter)
+        print("Number of hits is {}".format(hit_counter))
         print("Average is {}".format(sum(gene_symbol_counter.values())/len(gene_symbol_counter.values())))
         print("Max is {}".format(max(gene_symbol_counter.values())))
         print("Min is {}".format(min(gene_symbol_counter.values())))
@@ -60,6 +65,8 @@ def read_fasta_and_filter(fasta_file, gene_file):
            writer.writerow(["num_hits"])
            writer.writerows(map(lambda x : [str(x)], gene_symbol_counter.values()))
            print("Wrote hits to csv, so we can visualize later")
+
+        return [fasta_df, total_sequence_aggregator]
 
 def read_gene_file(gene_file):
     with open(gene_file) as f:
@@ -74,6 +81,31 @@ def read_gene_file(gene_file):
 
         return gene_names
 
+def write_fasta_file(fasta_df):
+    with open("./data/hsapiens/Homo_sapiens.cds.andreev.fa", "w") as outfile:
+        writer = csv.writer(outfile)
+        for fasta in fasta_df:
+            writer.writerow(fasta)
+    return
+
+def calculate_nucleotide_freqs(total_seq):
+    count_dict = {}
+    for letter in total_seq:
+        if letter not in count_dict:
+            count_dict[letter] = 0
+        count_dict[letter] += 1
+
+    del count_dict["N"]
+
+    total = count_dict["A"] + count_dict["T"] + count_dict["C"] + count_dict["G"]
+    freq_dict = {k: v / float(total) for k, v in count_dict.items()}
+    print(freq_dict.items())
+
 fasta_file = "./data/hsapiens/Homo_sapiens.GRCh38.cds.all.fa"
 gene_file  = "./data/hsapiens/andreev_genes.csv"
-read_fasta_and_filter(fasta_file, gene_file)
+print(" --- Reading FASTA and filtering only for D.E. Andreev genes ---")
+[fasta_df, total_seq] = read_fasta_and_filter(fasta_file, gene_file)
+print(" --- Writing csv with FASTA values for those filtered genes  --- ")
+write_fasta_file(fasta_df)
+print(" --- Calculating background nucleotide frequencies for all genes ---")
+calculate_nucleotide_freqs(total_seq)
